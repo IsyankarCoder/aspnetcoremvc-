e4d03be4e6c5cbd967598874ed94ab74d6665bc1
Volkan GENÇ <gencvolkan@gmail.com>
2020-08-15 14:16:23 -0700

    Cascade

--- Database/EfDbContext.cs
+++ Database/EfDbContext.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using test_mvc_app.Models;
+using System.Linq;
 
 namespace test_mvc_app.Database
 {
@@ -19,7 +20,12 @@ namespace test_mvc_app.Database
       DbSet<Employee> Employee {get;set;}
 
       protected override void OnModelCreating(ModelBuilder modelBuilder){
+
         base.OnModelCreating(modelBuilder);
+
+        foreach(var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e=>e.GetForeignKeys())){
+          foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+        }
 
       }
        
---
+++ common/DeleteBehaviour.cs
@@ -0,0 +1,6 @@
+public enum DeleteBehaviour{
+    ClientSetNull=0,
+    Restrict=1,
+    SetNull=2,
+    Cascade=3
+}
